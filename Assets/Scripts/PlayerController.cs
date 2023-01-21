bd10987664bd6e6fdeeb863086f063eacb9420af
Oodum <adamtam2004@outlook.com>
2023-01-21 17:20:10 -0500

    Initial Unity commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Camera cam;
+    [SerializeField] Animator playerAnimator;
+
+    [SerializeField] float jumpHeight = 4f;
+    [SerializeField] float walkSpeed = 5f;
+
+    PlayerInput inputAction;
+
+    Vector2 direction;
+    Vector2 lookVector;
+    Vector3 cameraRotation;
+
+    private float distanceToGround;
+    private bool isGrounded;
+    private bool isWalking = false;
+
+    private void Awake() {
+        inputAction = new PlayerInput();
+
+        inputAction.Player.Move.performed += cntxt => direction = cntxt.ReadValue<Vector2>();
+        inputAction.Player.Move.canceled += cntxt => direction = Vector2.zero;
+
+        inputAction.Player.Jump.performed += cntxt => Jump();
+
+        print("done");
+    }
+    private void OnEnable() {
+        
+    }
+    private void Update() {
+        
+    }
+    private void OnDisable() {
+        
+    }
+    private void Jump() { 
+    
+    }
+
+}
